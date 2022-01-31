17d642c168c6f1af5491b55c8128e4becb37d52f
Danil1997Dev <77538945+Danil1997Dev@users.noreply.github.com>
2022-01-31 15:01:50 +0800

    change project gen and adding system whith niosii

---
+++ Lab4/niosII/niosII_bb.v
@@ -0,0 +1,10 @@
+
+module niosII (
+	clk_clk,
+	reset_reset_n,
+	generator_export_fout);	
+
+	input		clk_clk;
+	input		reset_reset_n;
+	output		generator_export_fout;
+endmodule
---
+++ Lab4/niosII/niosII_inst.vhd
@@ -0,0 +1,15 @@
+	component niosII is
+		port (
+			clk_clk               : in  std_logic := 'X'; -- clk
+			reset_reset_n         : in  std_logic := 'X'; -- reset_n
+			generator_export_fout : out std_logic         -- fout
+		);
+	end component niosII;
+
+	u0 : component niosII
+		port map (
+			clk_clk               => CONNECTED_TO_clk_clk,               --              clk.clk
+			reset_reset_n         => CONNECTED_TO_reset_reset_n,         --            reset.reset_n
+			generator_export_fout => CONNECTED_TO_generator_export_fout  -- generator_export.fout
+		);
+
